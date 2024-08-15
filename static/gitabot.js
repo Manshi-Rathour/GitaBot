2b9a0eccbdccd6900f8465ea3de580091ffa0e9c
Manshi Rathour <118745353+Manshi-Rathour@users.noreply.github.com>
2024-08-15 16:31:09 +0530

    created seperate files for navbar

--- static/gitabot.js
+++ static/gitabot.js
@@ -1,16 +1,5 @@
 console.log("Chatbot script loaded");
 
-
-// For navbar scroll
-$(document).ready(function () {
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 50) {
-            $('.custom-navbar').addClass('navbar-scroll');
-        } else {
-            $('.custom-navbar').removeClass('navbar-scroll');
-        }
-    });
-});
 function openNewChat() {
     location.reload();
 }
